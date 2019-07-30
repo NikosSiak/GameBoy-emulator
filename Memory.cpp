4780e72e68d18016aca934eff62e8df2f158e057
NikosSiak <nikos.siakas@gmail.com>
2019-07-30 17:45:54 +0300

    loaded bootstrap code to ram

--- Memory.cpp
+++ Memory.cpp
@@ -2,4 +2,20 @@
 // Created by nikos on 29-Jul-19.
 //
 
+#include <algorithm>
+#include <fstream>
 #include "Memory.hpp"
+
+Memory::Memory() {
+
+    std::ifstream file("resources/bootstrap.bin", std::ios::binary | std::ios::ate);
+    std::fill_n(ram, 0xFFFF, 0);
+
+    if (file.good()){
+        int fsize = file.tellg();
+        file.seekg(0, std::ios::beg);
+        file.read(reinterpret_cast<char *>(ram), fsize);
+        file.close();
+    }
+
+}
