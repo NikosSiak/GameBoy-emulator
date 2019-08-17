7e72fdee6271870c2de8ea7ab622e1ccb002646d
NikosSiak <nikos.siakas@gmail.com>
2019-08-17 16:45:35 +0300

    changed bitoperations to inline functions

--- src/util/bitoperations.hpp
+++ src/util/bitoperations.hpp
@@ -7,10 +7,16 @@
 
 #include <cstdint>
 
-uint8_t setBit(uint8_t byte, int bit);
+inline uint8_t setBit(uint8_t byte, int bit) {
+    return byte | (1 << bit);
+}
 
-uint8_t resetBit(uint8_t byte, uint8_t bit);
+inline uint8_t resetBit(uint8_t byte, uint8_t bit) {
+    return byte & ~(1 << bit);
+}
 
-bool checkBit(uint8_t byte, uint8_t bit);
+inline bool checkBit(uint8_t byte, uint8_t bit) {
+    return ((byte >> bit) & 1) == 1;
+}
 
 #endif //GAMEBOY_EMULATOR_BITOPERATIONS_HPP
